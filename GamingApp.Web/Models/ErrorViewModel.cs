f1ce8b9c6eb0b787b54694ce64593be5bdabf992
Todor Mitevski <todormitevski5@gmail.com>
2024-07-31 14:47:11 +0200

    Add project files.

---
+++ GamingApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GamingApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
